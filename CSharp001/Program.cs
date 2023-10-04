7656c81de71dc30aaa17479b654e694360224b78
c2158 <c2158@oic.jp>
2023-10-04 14:58:18 +0900

    first commit

---
+++ CSharp001/Program.cs
@@ -0,0 +1,22 @@
+﻿/* swap */
+using System;
+
+namespace CSharp001
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int a = 10, b = 20, tmp;
+
+            tmp = a;
+            a = b;
+            b = tmp;
+
+            Console.Write("a={0}  b={1}\n", a, b);
+
+            Console.ReadKey();
+        }
+    }
+}
